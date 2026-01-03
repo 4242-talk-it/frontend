c5bf9d134aecc2bfc87564484f12cba25a1e7fe4
chlqhrud <kelly07192@gmail.com>
2026-01-03 21:17:56 +0900

    feat(app) : 앱 진입점 및 프로바이더 연결 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/Router';
 import './index.css';
-import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
