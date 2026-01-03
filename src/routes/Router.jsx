5e511ace2edfb681d2f32d21a00c2abe34d71e86
chlqhrud <kelly07192@gmail.com>
2026-01-03 21:17:03 +0900

    feat(router) : Authenticated 라우터 추가 및 라우터 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,50 +1,101 @@
 import { createBrowserRouter } from 'react-router-dom';
-
+import App from '../App'; // App 컴포넌트 임포트 확인!
 import LandingPage from "../component/LandingPage";
 import SaisaiHome from "../component/SaisaiHome";
 import Login from "../component/Login";
 import Signup from "../component/Signup";
-import ChattingPage from "../component/ChattingPage";
+import ChattingPage from "../component/ChattingPage"; 
+import CommunityPage from "../pages/community/CommunityPage";
 import CommunityCreate from "../pages/community/CommunityCreate";
 import CommunityDetail from "../pages/community/CommunityDetail";
-import CommunityPage from "../pages/community/CommunityPage";
-import AIChattingPage from "../component/AIChattingPage";
 import MyPage from "../component/MyPage";
-import Header from "../component/Header";
+import AIChattingPage from "../component/AIChattingPage";
+import AuthenticatedRoute from "./AuthenticatedRoute";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <LandingPage type="header" />,
-  },
-  {
-    path: '/community',
+    element: <App />,
     children: [
-        {
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'signup',
+        element: <Signup />,
+      },
+      {
+        path: 'home',
+        element: (
+          <AuthenticatedRoute>
+            <SaisaiHome />
+          </AuthenticatedRoute>
+        ),
+      },
+      {
+        path: 'mypage',
+        element: (
+          <AuthenticatedRoute>
+            <MyPage />
+          </AuthenticatedRoute>
+        ),
+      },
+      {
+        path: 'chatting',
+        element: (
+          <AuthenticatedRoute>
+            <ChattingPage />
+          </AuthenticatedRoute>
+        ),
+      },
+      {
+        path: 'aichat',
+        element: (
+          <AuthenticatedRoute>
+            <AIChattingPage />
+          </AuthenticatedRoute>
+        ),
+      },
+      // 커뮤니티 그룹화
+      {
+        path: 'community',
+        children: [
+          {
             index: true,
             element: (
-            <AuthenticatedRoute>
-                <Refrigerator />
-            </AuthenticatedRoute>
+              <AuthenticatedRoute>
+                <CommunityPage />
+              </AuthenticatedRoute>
             ),
-        },
-        {
-            path: '',
+          },
+          {
+            path: 'create',
             element: (
-            <AuthenticatedRoute>
-                <Refrigerator />
-            </AuthenticatedRoute>
+              <AuthenticatedRoute>
+                <CommunityCreate />
+              </AuthenticatedRoute>
             ),
-        },
-        {
-            path: 'search',
+          },
+          {
+            path: 'detail',
             element: (
-            <AuthenticatedRoute>
-                <RefrigeratorSearch />
-            </AuthenticatedRoute>
+              <AuthenticatedRoute>
+                <CommunityDetail />
+              </AuthenticatedRoute>
             ),
-        },
+          },
+        ],
+      },
     ],
+  },
+  {
+    path: '*',
+    element: <LandingPage />, // 정의되지 않은 경로는 랜딩페이지로
   },
 ]);
 
