02210ac036727a39e11002583c352fd1c2dd4a80
SeongEunY <tjddms0116@naver.com>
2026-02-04 18:52:29 +0900

    [feat]: 최근 메시지 하단 스크롤 및 실시간 매칭 연동

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'window', // 아까 설정한 global 문제 해결용
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 스프링 부트 주소
+        changeOrigin: true,
+        secure: false,
+      },
+      '/ws': {
+        target: 'http://localhost:8080',
+        ws: true, // 웹소켓 프록시 설정
+      },
+    },
+  },
 })
