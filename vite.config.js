90dce961aae06e0af955cee1b7937994f6332998
SeongEunY <tjddms0116@naver.com>
2026-02-13 03:27:51 +0900

    [comment]: 주석 삭제

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: 'window', // 아까 설정한 global 문제 해결용
+    global: 'window',
   },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // 스프링 부트 주소
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
       '/ws': {
         target: 'http://localhost:8080',
-        ws: true, // 웹소켓 프록시 설정
+        ws: true,
       },
     },
   },
